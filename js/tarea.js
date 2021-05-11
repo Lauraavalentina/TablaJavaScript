6115ac1c1bb34c6c7604c84c2732cca992629373
Lauraavalentina <laura4mp@gmail.com>
2021-05-11 15:20:32 -0500

    Tarea Tabla

---
+++ js/tarea.js
@@ -0,0 +1,11 @@
+for (let i = 2; i <= 20; i++, i++) {
+  let table = 
+     `<tr>
+      <th scope="row"> ${i}</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>`;
+  
+  document.getElementById("table").innerHTML += table;
+  }
